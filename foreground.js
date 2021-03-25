a70955faf7b5b3ec2485fa6f49fa8eb8dfd98cef
as2782 <42718608+as2782@users.noreply.github.com>
2021-03-25 15:36:05 -0400

    Add files via upload
    pop up shows icon

---
+++ foreground.js
@@ -0,0 +1,11 @@
+// console.log('from foreground')
+const first = document.createElement('button');
+first.innerText = "SET DATA";
+first.id = "first";
+
+const second = document.createElement('button');
+second.innerText = "SHOUTOUT";
+second.id = "second";
+
+document.querySelector('body').appendChild(first)
+document.querySelector('body').appendChild(second)
