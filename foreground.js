f931acc80e54c864130ca01c1542b34cabc4d94d
as2782 <as2782@cornell.edu>
2021-04-01 17:01:47 -0400

    icons better, overrides new page

--- foreground.js
+++ foreground.js
@@ -3,9 +3,27 @@ const first = document.createElement('button');
 first.innerText = "SET DATA";
 first.id = "first";
 
-const second = document.createElement('button');
-second.innerText = "SHOUTOUT";
-second.id = "second";
+// const second = document.createElement('button');
+// second.innerText = "SHOUTOUT";
+// second.id = "second";
 
-document.querySelector('body').appendChild(first)
-document.querySelector('body').appendChild(second)
+// document.querySelector('body').appendChild(first)
+// document.querySelector('body').appendChild(second)
+
+document.querySelector('body').appendChild(first);
+
+first.addEventListener('click', () => {
+  // you can store by doing .sync, but that syncs so that you have to login 
+  // whereas local stores it on their device basically
+  chrome.storage.local.set({ "password": "123" });
+  console.log("I SET DATA");
+})
+
+document.querySelector('#go-to-options').addEventListener(function () {
+  console.log("openned")
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
