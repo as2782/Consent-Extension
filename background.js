3e40ca5986083d24982028d5cddf44ec6aea5e81
as2782 <as2782@cornell.edu>
2021-04-05 20:32:24 -0400

    it monitors every website you go to and for now displays the title in the background page that you open from the extension page. still figuring out how to transfer data from storage onto an html page

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
 chrome.tabs.onActivated.addListener(tab => {
   chrome.tabs.get(tab.tabId, current_tab_info => {
-    // This would just show you which url in the background
-    // console.log(current_tab_info.url)
-    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      chrome.tabs.insertCSS(null, { file: './mystyles.css' });
-      chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
+    // Only collect tab title if it is not "New Tab", there are probably other 
+    //edge cases as well
+    if (/[^New Tab]/.test(current_tab_info.title)) {
+      chrome.storage.local.set({ "current_title": current_tab_info.title }, function () {
+        //this is just to see that it's working in "background page" on extension page
+        console.log('Title is set to ' + current_tab_info.title);
+      });
     }
+
+    // if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
+    //   chrome.tabs.insertCSS(null, { file: './mystyles.css' });
+    //   chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
+    // }
   });
 });
 
