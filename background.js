6882c1a812c027cbc41d89016353e6c5943e72de
ernafflu4 <ernafflu4@gmail.com>
2021-04-27 21:29:31 -0400

    updated extension, added comments

--- background.js
+++ background.js
@@ -1,67 +1,58 @@
-var history_lst = [];
-var urls_lst = [];
 // The object that will be stored in local storage
-// Eventually will need to not overwrite this with each update of extension
+// It will contain websites visited and dates of visit
 var all_lst = {};
 
-// chrome.tabs.onActivated.addListener(tab => {
-//   chrome.tabs.get(tab.tabId, current_tab_info => {
-//     // Only collect tab title if it is not "New Tab", there are probably other 
-//     //edge cases as well
-//     if (/[^New Tab]/.test(current_tab_info.title)) {
-//       history_lst.push(current_tab_info.title);
-//       chrome.storage.local.set({ "title_list": history_lst }, function () {
-//         //this is just to see that it's working in "background page" on extension page
-//         console.log('Title is set to ' + current_tab_info.title);
-//         console.log('title list is: ' + history_lst);
-//       });
-//     }
-
-//     // if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-//     //   chrome.tabs.insertCSS(null, { file: './mystyles.css' });
-//     //   chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
-//     // }
-//   });
-// });
-
-// This will update every time a page is updated (refreshed, or changes in some way)
+// This listens on every time a page is updated (refreshed or changed)
 // Does not listen on whether a tab or browser is first opened
 chrome.tabs.onUpdated.addListener(tab => {
   // This line looks at the active window
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     // console.log(tabs[0].title); // just a test; .title can be used for watch words later
-    // Looks like tabs[0] looks at the tab with tab id 0, which is the active tab?
+    // Looks like tabs[0] looks at the tab with tab id 0, which is the active tab
     let url = tabs[0].url;
-    // The previous line takes the whole url. These lines convert it to a URL object
+
+    // The previous line takes the whole url. The next lines convert it to a URL object
     // And truncates the object to just to 'www.domain.whatever'
     // In instances where 'www' or '.whatever' is missing, just returns 'domain'
     let domain = new URL(url);
     domain = domain.hostname;
+
     // When any update happens, if a url exists, we take the current date and time
-    if (url) {
+    // But only if the url is not for the extension itself, the extensions page, or a newly opened tab
+    if (domain == chrome.runtime.id | domain == "newtab" | domain == "extensions") {
+      // do nothing
+    }
+    else if (url) {
       let date = new Date();
       // Converting date to mm/dd/yy
       date = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
       // Checking if this site has already been accessed before
       // If it has, we do not add it to the object that has all accessed domains
       if (!(all_lst.hasOwnProperty(domain))) {
-        all_lst[domain] = String(date); //it seems we have to convert these to string to print out to console after getting from local storage
-        //console.log(all_lst);
-        // Adding this info to local storage
+        // Converting the Date object to a string so it can be displayed properly
+        all_lst[domain] = String(date);
+
+        // There is a bug where storage is overwritten after extension updates
+        // The below code is attempting to fix this.
+
+        // try {
+        //   chrome.storage.local.get('browse_info', function (result) {
+        //     let obj = result.browse_info;
+        //     let merged = Object.assign(result, all_lst);
+        //     all_lst = merged;
+        //   });
+        // }
+        // catch(err) {
+        //   // pass - do nothing here
+        // }
+
+        // Adding the website and date info to local storage
         chrome.storage.local.set({'browse_info': all_lst}, function() {
-          //console.log(all_lst);
+          // empty function call
         });
       }
     }
-
-    // urls_lst.push(domain);
-    
-
-    
   });
-  // Just for testing purposes
-  // chrome.storage.local.get('browse_info', function (result) {
-  //   var lst = result.browse_info;
-  //   console.log(lst);
-  // });
 });
+
+
--- index.js
+++ index.js
@@ -1,64 +1,86 @@
-// function n_lst() {
-//   var lst = chrome.storage.local.get('title_list', function (result) {
-//     console.log("hi" + lst);
-//   });
-//   return lst;
-// }
+// Hardcoding the list of websites and privacy policy information for now. 
+// Calling it 'dummy'. Will pull from server in final implementation
+const dummy = {
+  "www.google.com": 
+  {
+    "start_url": "Privacy Policy Link",
+    "final_url": "Google Privacy Policy Link",
+    "text": "This is Google's Privacy Policy Info"
+  },
+  "www.reddit.com": 
+  {
+    "start_url": "Privacy Policy Link",
+    "final_url": "Reddit Privacy Policy Link",
+    "text": "This is Reddit's Privacy Policy Info"
+  },
+  "www.youtube.com": 
+  {
+    "start_url": "Privacy Policy Link",
+    "final_url": "Youtube Privacy Policy Link",
+    "text": "This is YouTube's Privacy Policy Info"
+  },
+  "www.facebook.com": 
+  {
+    "start_url": "Privacy Policy Link",
+    "final_url": "Facebook Privacy Policy Link",
+    "text": "This is Facebook's Privacy Policy Info"
+  },
+  "www.tumblr.com": 
+  {
+    "start_url": "Privacy Policy Link",
+    "final_url": "Tumblr Privacy Policy Link",
+    "text": "This is Tumblr's Privacy Policy Info"
+  }
+}
 
+// Will use the fetch api to pull info from server.
+// Code is commented out, but will be the following:
+// fetch('http://server.com/file.json')
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+// No need to console.log the data in the previous line.
+
+// The below will take get the information currently in local storage
+// We are storing everything in an object called 'browse_info'
+// And pulling that out of the argument we name 'result' below
 chrome.storage.local.get('browse_info', function (result) {
   var lst = result.browse_info;
-  // document.getElementById("table").innerHTML = result.browse_info['extensions'];
+  // In index.html, the table has id "table". 
+  // The code below creates new rows there.
   let table_ref = document.getElementById("table");
+  // 'lst' is the 'browse_info' object from before
+  // for each entry, which we call 'key' we add a row
   for (const key in lst) {
+    // Insert new empty row
   	let new_row = table_ref.insertRow(-1);
 
+    // Insert new cells for each relevant field
   	let new_cell = new_row.insertCell(0);
   	let cell_date = new_row.insertCell(1);
+    let cell_info = new_row.insertCell(2);
+    let cell_link = new_row.insertCell(3);
 
+    // Create Text to add to each new cell
   	let new_text = document.createTextNode(key);
   	let text_date = document.createTextNode(lst[key]);
 
+    // If the current website domain is not in our external file,
+    // We display "Not Found" in the appopriate extension fields
+    // Otherwise, we get that information and display it
+    if (dummy[key] == undefined){
+      var text_info = document.createTextNode("Not Found");
+      var text_link = document.createTextNode("Not Found");
+    }
+    else {
+      var text_info = document.createTextNode(dummy[key]["text"]);
+      var text_link = document.createTextNode(dummy[key]["start_url"]);
+    }
+
+    // Add Text to new cells
   	new_cell.appendChild(new_text);
   	cell_date.appendChild(text_date);
+    cell_info.appendChild(text_info);
+    cell_link.appendChild(text_link);
   }
 });
 
-// chrome.storage.local.get('title_list', function (result) {
-//   
-//   // console.log('All Titles ' + result); 
-// });
-
-// let mountains = [
-//   { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
-//   { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
-//   { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
-//   { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
-//   { name: "Monte Amiata", height: 1738, place: "Siena" }
-// ];
-
-// function generateTableHead(table, data) {
-//   let thead = table.createTHead();
-//   let row = thead.insertRow();
-//   for (let key of data) {
-//     let th = document.createElement("th");
-//     let text = document.createTextNode(key);
-//     th.appendChild(text);
-//     row.appendChild(th);
-//   }
-// }
-
-// function generateTable(table, data) {
-//   for (let element of data) {
-//     let row = table.insertRow();
-//     for (key in element) {
-//       let cell = row.insertCell();
-//       let text = document.createTextNode(element[key]);
-//       cell.appendChild(text);
-//     }
-//   }
-// }
-
-// let table = document.querySelector("table");
-// let data = Object.keys(mountains[0]);
-// generateTableHead(table, data);
-// generateTable(table, mountains);
