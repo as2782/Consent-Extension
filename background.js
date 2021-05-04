1e724bb5e83888faea1061e628bfe57eb4568745
as2782 <as2782@cornell.edu>
2021-05-04 17:15:50 -0400

    why isnt this working

--- background.js
+++ background.js
@@ -5,15 +5,15 @@ var all_lst = {};
 
 // The below will ensure that previous browsing history is not overwritten
 // after ending and starting a new browsing session
-try {
-  chrome.storage.local.get('browse_info', function (result) {
-    let obj = result.browse_info;
-    all_lst = obj;
-  });
-}
-catch(error) {
-  // pass - do nothing here
-}
+// try {
+//   chrome.storage.local.get('browse_info', function (result) {
+//     let obj = result.browse_info;
+//     all_lst = obj;
+//   });
+// }
+// catch (error) {
+//   // pass - do nothing here
+// }
 
 
 var titles = {};
@@ -27,47 +27,52 @@ chrome.tabs.onUpdated.addListener(tab => {
     // console.log(tabs[0].title); // just a test; .title can be used for watch words later
     // Looks like tabs[0] looks at the tab with tab id 0, which is the active tab
     let url = tabs[0].url;
-
+    let curr_title = tabs[0].title;
     // The previous line takes the whole url. The next lines convert it to a URL object
     // And truncates the object to just to 'www.domain.whatever'
     // In instances where 'www' or '.whatever' is missing, just returns 'domain'
     let domain = new URL(url);
     domain = domain.hostname;
+    // date formatting
+    let date = new Date();
+    // Converting date to mm/dd/yy hh:mm
+    // any variable using slice() below is having a leading 0 added when needed
+    let date_month = ('0' + String(date.getMonth() + 1)).slice(-2)
+    let date_day = ('0' + String(date.getDate())).slice(-2)
+    let date_year = String(date.getFullYear())
+    let date_hour = String(date.getHours())
+    let date_mins = ('0' + String(date.getMinutes())).slice(-2)
+    date = date_month + '/' + date_day + '/' + date_year + ', ' + date_hour + ':' + date_mins;
 
     // When any update happens, if a url exists, we take the current date and time
     // But only if the url is not for the extension itself, the extensions page, or a newly opened tab
     // update means: someone refreshes OR clicks on something on the page OR 
     // makes a search
     if (domain == chrome.runtime.id | domain == "newtab" | domain == "extensions") {
-      // do nothing
+      // do nothing, same logic as with url below
     }
-    else if (url) {
-      let date = new Date();
-      // Converting date to mm/dd/yy hh:mm
-      // any variable using slice() below is having a leading 0 added when needed
-      let date_month = ('0' + String(date.getMonth() + 1)).slice(-2)
-      let date_day = ('0' + String(date.getDate())).slice(-2)
-      let date_year = String(date.getFullYear())
-      let date_hour = String(date.getHours())
-      let date_mins = ('0' + String(date.getMinutes())).slice(-2)
-      date =  date_month + '/' + date_day + '/' + date_year + ', ' + date_hour + ':' + date_mins;
+    else {
       // Checking if this site has already been accessed before
       // If it has, we do not add it to the object that has all accessed domains
+      // console.log("title beginning of else: " + curr_title);
       if (!(all_lst.hasOwnProperty(domain))) {
         // Converting the Date object to a string so it can be displayed properly
         all_lst[domain] = String(date);
-
-
-
+        // adding date to the list of titles
+        console.log("added " + curr_title + " to list of titles ");
+        titles[curr_title] = String(date);
         // Adding the website and date info to local storage
         chrome.storage.local.set({ 'browse_info': all_lst }, function () {
           // empty function call
+          console.log("url is " + url);
         });
-        // Adding the website title to local storage to match against watch words
-        titles.hasOwnProperty(tabs[0].title);
-        chrome.storage.local.set({ 'title': titles }, function () {
-          // empty function call 
-        });
+        // Adding the website and date info to local storage only if it's not the urls
+        if (!(curr_title.includes("http"))) {
+          chrome.storage.local.set({ 'title': titles }, function () {
+            // empty function call
+            console.log("title in storage " + curr_title);
+          });
+        }
       }
     }
   });
--- index.js
+++ index.js
@@ -39,10 +39,10 @@ const dummy = {
 //   })
 
 // list of alert words to check the titles against
-var alerts = ["porn", "drug", "weed", "pot", "suicide", "Drug", "sex", "cannabis",
-  "hashish", "hemp", "herb", "tea", "help", "ganja", "hash", "joint", "reefer",
-  "doobie", "loco weed", "Porno", "raunchy", "steamy", "sexy", "X-rated", "xxx",
-  "self mutilation", "self-mutilation", "guns", "qanon", "Qanon", "self-injury"];
+var alerts = ["Porn", "Drug", "Weed", "Pot", "Suicide", "Drugs", "Sex", "Cannabis",
+  "Hashish", "Hemp", "Herb", "Tea", "Help", "Ganja", "Hash", "Joint", "Reefer",
+  "Doobie", "Loco Weed", "Porno", "Raunchy", "Steamy", "Sexy", "X-Rated", "xxx", "XXX", "Suicidal",
+  "Self Mutilation", "Self-Mutilation", "Guns", "QAnon", "qanon", "Qanon", "Self-Injury", "marijuana"];
 
 // Will use the fetch api to pull info from server.
 // Code is commented out, but will be the following:
@@ -55,91 +55,103 @@ var alerts = ["porn", "drug", "weed", "pot", "suicide", "Drug", "sex", "cannabis
 // We are storing everything in an object called 'browse_info'
 // And pulling that out of the argument we name 'result' below
 chrome.storage.local.get('browse_info', function (result) {
-  chrome.storage.local.get('title', function (result2) {
-    var title_lst = result2.title;
-    var lst = result.browse_info;
+  var lst = result.browse_info;
 
-    // This section adds browsing history to a list, to sort by time accessed
-    var sort_lst = []
-    for (obj in lst){
-      sort_lst.push([lst[obj], obj])
+  // This section adds browsing history to a list, to sort by time accessed
+  var sort_lst = []
+  for (obj in lst) {
+    sort_lst.push([lst[obj], obj])
+  }
+  sort_lst.sort()
+  //console.log(sort_lst)
+
+  // In index.html, the table has id "table". 
+  // The code below creates new rows there.
+  let table_ref = document.getElementById("table");
+  // 'lst' is the 'browse_info' object from before
+  // for each entry, which we call 'key' we add a row
+  for (const key in lst) {
+    // Insert new empty row
+    let new_row = table_ref.insertRow(-1);
+
+    // Insert new cells for each relevant field
+    let new_cell = new_row.insertCell(0);
+    let cell_date = new_row.insertCell(1);
+    let cell_info = new_row.insertCell(2);
+    let cell_link = new_row.insertCell(3);
+
+    // Create Text to add to each new cell
+    let new_text = document.createTextNode(key);
+    let text_date = document.createTextNode(lst[key]);
+
+    // If the current website domain is not in our external file,
+    // We display "Not Found" in the appopriate extension fields
+    // Otherwise, we get that information and display it
+
+    if (dummy[key] == undefined) {
+      var text_info = document.createTextNode("Not Found");
+      var text_link = document.createTextNode("Not Found");
     }
-    sort_lst.sort()
-    //console.log(sort_lst)
-
-    // In index.html, the table has id "table". 
-    // The code below creates new rows there.
-    let table_ref = document.getElementById("table");
-    // 'lst' is the 'browse_info' object from before
-    // for each entry, which we call 'key' we add a row
-    for (const key in lst) {
-      // Insert new empty row
-      let new_row = table_ref.insertRow(-1);
-
-      // Insert new cells for each relevant field
-      let new_cell = new_row.insertCell(0);
-      let cell_date = new_row.insertCell(1);
-      let cell_info = new_row.insertCell(2);
-      let cell_link = new_row.insertCell(3);
-      let cell_alert = new_row.insertCell(4);
-
-      // Create Text to add to each new cell
-      let new_text = document.createTextNode(key);
-      let text_date = document.createTextNode(lst[key]);
-
-      // If the current website domain is not in our external file,
-      // We display "Not Found" in the appopriate extension fields
-      // Otherwise, we get that information and display it
-
-      if (dummy[key] == undefined) {
-        var text_info = document.createTextNode("Not Found");
-        var text_link = document.createTextNode("Not Found");
-      }
-      else {
-        var text_info = document.createTextNode(dummy[key]["text"]);
-        var text_link = document.createTextNode(dummy[key]["start_url"]);
-      }
-
-      // make a map of the json file so that its faster to search through it
-      // var policyMap = {};
-      // var i = null;
-      // for (i = 0; coppa_policies.length > i; i += 1) {
-      //   policyMap[coppa_policies[i].start_url] = coppa_policies[i];
-      // }
-
-      // var hasPolicy = function (curr_url) {
-      //   return policyMap[curr_url];
-      // };
-
-      // if (hasPolicy(key) != null) {
-      //   var text_info = document.createTextNode(hasPolicy(key).text);
-      //   var text_link = document.createTextNode(hasPolicy(key).final_url);
-      // }
-      // else {
-      //   var text_info = document.createTextNode("Not Found");
-      //   var text_link = document.createTextNode("Not Found");
-      // }
-
-      //check if the title contains any alert words
-      curr_title = key;
-      if (alerts.some(function (v) { return curr_title.indexOf(v) >= 0; })) {
-        // There's at least one
-        // console.log("found at v " + v);
-        var text_alert = document.createTextNode("An alert was found on this site");
-      }
-      else {
-        var text_alert = document.createTextNode("None");
-      }
-
-      // var text_alert = "dummy for now"
-      // Add Text to new cells
-      new_cell.appendChild(new_text);
-      cell_date.appendChild(text_date);
-      cell_info.appendChild(text_info);
-      cell_link.appendChild(text_link);
-      cell_alert.appendChild(text_alert);
+    else {
+      var text_info = document.createTextNode(dummy[key]["text"]);
+      var text_link = document.createTextNode(dummy[key]["start_url"]);
     }
-  });
 
+    // make a map of the json file so that its faster to search through it
+    // var policyMap = {};
+    // var i = null;
+    // for (i = 0; coppa_policies.length > i; i += 1) {
+    //   policyMap[coppa_policies[i].start_url] = coppa_policies[i];
+    // }
+
+    // var hasPolicy = function (curr_url) {
+    //   return policyMap[curr_url];
+    // };
+
+    // if (hasPolicy(key) != null) {
+    //   var text_info = document.createTextNode(hasPolicy(key).text);
+    //   var text_link = document.createTextNode(hasPolicy(key).final_url);
+    // }
+    // else {
+    //   var text_info = document.createTextNode("Not Found");
+    //   var text_link = document.createTextNode("Not Found");
+    // }
+
+    // var text_alert = "dummy for now"
+    // Add Text to new cells
+    new_cell.appendChild(new_text);
+    cell_date.appendChild(text_date);
+    cell_info.appendChild(text_info);
+    cell_link.appendChild(text_link);
+  }
+});
+
+chrome.storage.local.get('title', function (result) {
+  var title_lst = result.title;
+  // get the right table
+  let table_ref = document.getElementById("alert_table");
+  // iterate through all of the elements in title list
+  for (const key in title_lst) {
+    //check if the title contains any alert words
+    curr_title = key;
+    for (var a in alerts) {
+      if (curr_title.includes(alerts[a])) {
+        // if there is an alert, then make a new row in the table
+        let new_row = table_ref.insertRow(-1);
+        var text_alert = document.createTextNode("Alert Word: " + alerts[a]);
+        // Insert new cells for each relevant field
+        let new_cell = new_row.insertCell(0);
+        let cell_date = new_row.insertCell(1);
+        let cell_alert = new_row.insertCell(2);
+        // Create Text to add to each new cell
+        let new_text = document.createTextNode(key);
+        let text_date = document.createTextNode(title_lst[key]);
+        //Add the info to the table 
+        new_cell.appendChild(new_text);
+        cell_date.appendChild(text_date);
+        cell_alert.appendChild(text_alert);
+      }
+    }
+  }
 });
 
