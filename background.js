52825ca7cc44700f6987f8539139287882c7ca3a
maoxx243 <48844273+maoxx243@users.noreply.github.com>
2021-05-04 17:52:04 -0400

    comment the try catch thing in background.js

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/* Some code related to how to use Google login with Chrome Extensions is from:
+https://github.com/an-object-is-a/google-openid-connect-chrome-extension/blob/master/background.js
+*/
+
 // The object that will be stored in local storage
 // It will contain websites visited and dates of visit
 var all_lst = {};
@@ -5,15 +9,15 @@ var all_lst = {};
 
 // The below will ensure that previous browsing history is not overwritten
 // after ending and starting a new browsing session
-try {
-  chrome.storage.local.get('browse_info', function (result) {
-    let obj = result.browse_info;
-    all_lst = obj;
-  });
-}
-catch(error) {
-  // pass - do nothing here
-}
+// try {
+//   chrome.storage.local.get('browse_info', function (result) {
+//     let obj = result.browse_info;
+//     all_lst = obj;
+//   });
+// }
+// catch(error) {
+//   // pass - do nothing here
+// }
 
 
 var titles = {};
@@ -68,9 +72,83 @@ chrome.tabs.onUpdated.addListener(tab => {
         chrome.storage.local.set({ 'title': titles }, function () {
           // empty function call 
         });
+      } else if (all_lst===undefined) {
+        // handling the bug that all
       }
     }
   });
 });
 
 
+const CLIENT_ID = encodeURIComponent('347250747253-foraleeguhfi6firnbdmnui92on325ig.apps.googleusercontent.com');
+const RESPONSE_TYPE = encodeURIComponent('id_token');
+const REDIRECT_URI = encodeURIComponent('https://hblldpopcbinajlabijfdmongpamgame.chromiumapp.org')
+const SCOPE = encodeURIComponent('openid');
+const STATE = encodeURIComponent('amao');
+const PROMPT = encodeURIComponent('consent');
+
+let user_signed_in = false;
+
+function is_user_signed_in() {
+    return user_signed_in;
+}
+
+function create_auth_endpoint() {
+    let nonce = encodeURIComponent(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+
+    let openId_endpoint_url =
+        `https://accounts.google.com/o/oauth2/v2/auth
+?client_id=${CLIENT_ID}
+&response_type=${RESPONSE_TYPE}
+&redirect_uri=${REDIRECT_URI}
+&scope=${SCOPE}
+&state=${STATE}
+&nonce=${nonce}
+&prompt=${PROMPT}`;
+
+    return openId_endpoint_url;
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'login') {
+        if (user_signed_in) {
+            console.log("User is already signed in.");
+        } else {
+            chrome.identity.launchWebAuthFlow({
+                'url': create_auth_endpoint(),
+                'interactive': true
+            }, function (redirect_url) {
+                if (chrome.runtime.lastError) {
+                    // problem signing in
+                } else {
+                    let id_token = redirect_url.substring(redirect_url.indexOf('id_token=') + 9);
+                    id_token = id_token.substring(0, id_token.indexOf('&'));
+                    const user_info = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(id_token.split(".")[1]));
+
+                    if ((user_info.iss === 'https://accounts.google.com' || user_info.iss === 'accounts.google.com')
+                        && user_info.aud === CLIENT_ID) {
+                        console.log("User successfully signed in.");
+                        user_signed_in = true;
+                        chrome.browserAction.setPopup({ popup: './popup-signed-in.html' }, () => {
+                            sendResponse('success');
+                        });
+                    } else {
+                        // invalid credentials
+                        console.log("Invalid credentials.");
+                    }
+                }
+            });
+
+            return true;
+        }
+    } else if (request.message === 'logout') {
+        user_signed_in = false;
+        chrome.browserAction.setPopup({ popup: './popup.html' }, () => {
+            sendResponse('success');
+        });
+
+        return true;
+    } else if (request.message === 'isUserSignedIn') {
+        sendResponse(is_user_signed_in());
+    }
+});
