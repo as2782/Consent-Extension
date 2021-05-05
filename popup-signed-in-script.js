c2ec4f936824ed1b7c9ede0dccd8e490be2fbd69
maoxx243 <48844273+maoxx243@users.noreply.github.com>
2021-05-04 21:34:48 -0400

    comment popup-signed in

--- popup-signed-in-script.js
+++ popup-signed-in-script.js
@@ -1,11 +1,14 @@
+/* 
+Source from: https://github.com/an-object-is-a/google-openid-connect-chrome-extension/
+Author: an-object-is-a
+*/
+/* The code below send a message "logout" to background, 
+if the background response is "success", the popup window
+will close.
+*/
 document.querySelector('#sign-out').addEventListener('click', function () {
     chrome.runtime.sendMessage({ message: 'logout' }, function (response) {
         if (response === 'success') window.close();
     });
 });
 
-// document.querySelector('button').addEventListener('click', function () {
-//     chrome.runtime.sendMessage({ message: 'isUserSignedIn' }, function (response) {
-//         alert(response);
-//     });
-// });
